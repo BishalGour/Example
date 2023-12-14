1847cca5c09d99b8030f25f6cfcf901c51c16489
BishalGour <bishalgour2405@gmail.com>
2023-12-14 13:51:32 +0530

    change2

--- src/com/jsp/example/User.java
+++ src/com/jsp/example/User.java
@@ -1,5 +1,35 @@
 package com.jsp.example;
 
 public class User {
+	private int userId;
+	private String useremail;
+	private String userPassword;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getUseremail() {
+		return useremail;
+	}
+	public void setUseremail(String useremail) {
+		this.useremail = useremail;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	private String username ;
+
 
 }
